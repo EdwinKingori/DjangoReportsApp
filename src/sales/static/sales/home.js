e4618fed307c278f1f1f1fc84e2efa27ef1c818e
EDDY <eddmunyiri@gmail.com>
2024-05-13 20:51:46 +0300

    creating report objects

--- src/sales/static/sales/home.js
+++ src/sales/static/sales/home.js
@@ -2,7 +2,13 @@ console.log('Hello world!')
 
 const reportBtn = document.getElementById('report-btn')
 const img = document.getElementById('img') 
-const modalBody = document.getElementById('modal-body') 
+const modalBody = document.getElementById('modal-body')
+const reportForm = document.getElementById('report-form') 
+
+const reportName = document.getElementById('id_name')
+const reportRemarks = document.getElementById('id_remarks')
+const csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value
+console.log(csrf)
 
 console.log(reportBtn)
 console.log(img)
@@ -15,4 +21,13 @@ reportBtn.addEventListener('click', ()=>{
     console.log('clicked')
     img.setAttribute('class', 'w-100')
     modalBody.prepend(img)
+})
+
+reportForm.addEventListener('submit', e=>{
+    e.preventDefault()
+    const formData = new FormData()
+    formData.append('csrfmiddlewaretoken', csrf)
+    formData.append('name', reportName.value)
+    formData.append('remarks',reportRemarks.value)
+    formData.append('image', img.src)
 })
