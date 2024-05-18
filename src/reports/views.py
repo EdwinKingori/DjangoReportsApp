230bf2dd05297c0c61979293ba1570919e6b6e95
EDDY <eddmunyiri@gmail.com>
2024-05-18 23:11:34 +0300

    Create report objects

--- src/reports/views.py
+++ src/reports/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from profiles.models import Profile
 from django.http import JsonResponse
+from .utils import get_report_image
+from .models import Report
 
 # Create your views here.
 
@@ -11,6 +13,10 @@ def create_report_view(request):
         remarks = request.POST.get('remarks')
         image = request.POST.get('image')
 
+        img = get_report_image(image)
+
         author = Profile.objects.get(user=request.user)
+        Report.objects.create(name=name, remarks=remarks,
+                              image=img, author=author)
         return JsonResponse({'msg': 'send'})
     return JsonResponse({})
