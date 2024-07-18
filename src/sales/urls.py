19e68aabbf1b1d70acd47215a59b1e01be564852
eddmunyiri@gmail.com <eddmunyiri@gmail.com>
2024-07-18 17:39:01 +0300

    navbar configurations

--- src/sales/urls.py
+++ src/sales/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'sales'
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('list/', views.SaleListView.as_view(), name='list'),
+    path('sales/', views.SaleListView.as_view(), name='list'),
     path('sales/<pk>/', views.SaleDetailView.as_view(), name='sale_detail')
 ]
