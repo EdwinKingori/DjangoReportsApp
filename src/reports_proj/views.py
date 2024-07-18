12618fde6bfc88c4e4e7bb4e5bfe8a5e4638b349
eddmunyiri@gmail.com <eddmunyiri@gmail.com>
2024-07-18 17:50:43 +0300

    profiles and logout urls error handling

--- src/reports_proj/views.py
+++ src/reports_proj/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import AuthenticationForm
 
 def logout_view(request):
     logout(request)
-    return redirect(login)
+    return redirect('login')
 
 
 def login_view(request):
